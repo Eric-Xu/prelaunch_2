cd728ba65637e747749861cd83efa28d2c67a3e6
Eric-Xu <e.xulander@gmail.com>
2012-12-10 21:30:28 -0500

    Add validations and error messages

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -6,7 +6,7 @@ describe "Users" do
   describe "Sign up page" do
     before { visit root_path }
     it { should have_selector('h1', text: 'Sign up') }
-	it { should have_selector('title', text: 'Vouchflow | Sign up') }
+	  it { should have_selector('title', text: 'Vouchflow | Sign up') }
   end
 
   describe "Thank you page" do
@@ -14,4 +14,27 @@ describe "Users" do
     it { should have_selector('h1', text: 'Thank you') }
     it { should have_selector('title', text: 'Vouchflow | Thank you') }
   end 
+
+  describe "Signup process" do
+    before { visit root_path }
+
+    let(:submit) { "Notify Me" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
 end

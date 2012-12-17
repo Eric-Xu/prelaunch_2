05e1ff92ab84a2cea94f0b7e59dd1b350b018f9f
Eric-Xu <e.xulander@gmail.com>
2012-12-17 00:27:14 -0500

    Update text and css features

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -5,13 +5,13 @@ describe "Users" do
 
   describe "Sign up page" do
     before { visit root_path }
-    it { should have_selector('h1', text: 'Sign up') }
+    it { should have_selector('h1', text: 'Welcome') }
 	  it { should have_selector('title', text: 'Vouchflow | Sign up') }
   end
 
   describe "Thank you page" do
     before { visit thankyou_path }
-    it { should have_selector('h1', text: 'Thank you') }
+    it { should have_selector('h1', text: 'Thank You') }
     it { should have_selector('title', text: 'Vouchflow | Thank you') }
   end 
 
@@ -28,8 +28,8 @@ describe "Users" do
 
     describe "with valid information" do
       before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
+        fill_in "user_name",         with: "Example User"
+        fill_in "user_email",        with: "user@example.com"
       end
 
       it "should create a user" do
